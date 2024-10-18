75fa2dbc5c5e753b113118a7eeb40ade2e702021
Cody Olsen <stipsan@gmail.com>
2024-10-18 23:24:23 +0200

    translate timing

--- src/hooks/useTimeAgo.ts
+++ src/hooks/useTimeAgo.ts
@@ -1,63 +1,63 @@
-import { pluralize } from "@/lib/utils";
-import { useEffect, useState } from "react";
+import { pluralize } from '@/lib/utils'
+import { useEffect, useState } from 'react'
 
 export function useTimeAgo(date: string | number | Date): string {
-  const [timeAgo, setTimeAgo] = useState<string>("");
+  const [timeAgo, setTimeAgo] = useState<string>('')
 
   useEffect(() => {
     const updateTimeAgo = () => {
-      const now = new Date();
-      const past = new Date(date);
-      const diffInSeconds = Math.floor((now.getTime() - past.getTime()) / 1000);
+      const now = new Date()
+      const past = new Date(date)
+      const diffInSeconds = Math.floor((now.getTime() - past.getTime()) / 1000)
 
       switch (true) {
         case diffInSeconds < 60:
-          setTimeAgo("Nettopp");
-          break;
+          setTimeAgo('Now')
+          break
         case diffInSeconds < 3600: {
-          const minutes = Math.floor(diffInSeconds / 60);
-          setTimeAgo(`${minutes} min siden`);
-          break;
+          const minutes = Math.floor(diffInSeconds / 60)
+          setTimeAgo(`${minutes} min ago`)
+          break
         }
         case diffInSeconds < 3600 * 3: {
-          const minutes = Math.floor(diffInSeconds / 60) % 60;
-          const hours = Math.floor(diffInSeconds / 3600);
+          const minutes = Math.floor(diffInSeconds / 60) % 60
+          const hours = Math.floor(diffInSeconds / 3600)
           setTimeAgo(
-            `${hours} ${pluralize(hours, "time", "timer")} og ${minutes} min siden`,
-          );
-          break;
+            `${hours} ${pluralize(hours, 'hour', 'hours')} & ${minutes} min ago`
+          )
+          break
         }
         case diffInSeconds < 86400: {
-          const hours = Math.floor(diffInSeconds / 3600);
-          setTimeAgo(`${hours} ${pluralize(hours, "time", "timer")} siden`);
-          break;
+          const hours = Math.floor(diffInSeconds / 3600)
+          setTimeAgo(`${hours} ${pluralize(hours, 'hour', 'hours')} ago`)
+          break
         }
         case diffInSeconds < 172800: {
-          setTimeAgo("I går");
-          break;
+          setTimeAgo('Yesterday')
+          break
         }
         case diffInSeconds < 2592000: {
-          const days = Math.floor(diffInSeconds / 86400);
-          setTimeAgo(`${days} dager siden`);
-          break;
+          const days = Math.floor(diffInSeconds / 86400)
+          setTimeAgo(`${days} days ago`)
+          break
         }
         case diffInSeconds < 31536000: {
-          const months = Math.floor(diffInSeconds / 2592000);
-          setTimeAgo(`${months} mnd siden`);
-          break;
+          const months = Math.floor(diffInSeconds / 2592000)
+          setTimeAgo(`${months} months ago`)
+          break
         }
         default: {
-          const years = Math.floor(diffInSeconds / 31536000);
-          setTimeAgo(`${years} år siden`);
+          const years = Math.floor(diffInSeconds / 31536000)
+          setTimeAgo(`${years} year ago`)
         }
       }
-    };
+    }
 
-    updateTimeAgo();
-    const intervalId = setInterval(updateTimeAgo, 60000);
+    updateTimeAgo()
+    const intervalId = setInterval(updateTimeAgo, 60000)
 
-    return () => clearInterval(intervalId);
-  }, [date]);
+    return () => clearInterval(intervalId)
+  }, [date])
 
-  return timeAgo;
+  return timeAgo
 }
